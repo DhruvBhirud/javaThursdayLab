dac7f75b482ab3105b12ec580e7e6a82b4b5b34b
DhruvBhirud <bhiruddhruv@gmail.com>
2022-09-29 21:08:08 +0530

    Added 3,4,5,6 codes

---
+++ 02.LEC.29.9.2022/primeIn2DArray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class primeIn2DArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,count,n=150;
+        System.out.println("Prime numbers between 1 to 150 are ");
+        for(int j=2;j<=n;j++)
+        {
+        	count=0;
+        	for(i=1;i<=j;i++)
+        	{
+        		if(j%i==0)
+        		{
+        			count++;        
+        		}
+        	}
+        	if(count==2)
+        		System.out.print(j+"  ");     
+        }
+	}
+
+}
