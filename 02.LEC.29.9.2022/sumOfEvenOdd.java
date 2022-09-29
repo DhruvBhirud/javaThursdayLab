4eff4859596adcebeeeccceef014543b95a7423f
DhruvBhirud <bhiruddhruv@gmail.com>
2022-09-29 18:08:31 +0530

    Updated 29.9.2022 lab comments

--- 02.LEC.29.9.2022/sumOfEvenOdd.java
+++ 02.LEC.29.9.2022/sumOfEvenOdd.java
@@ -10,18 +10,24 @@ public class sumOfEvenOdd {
 		char ans;
 		Scanner sc=new Scanner(System.in);
 		do {
+			//Taking input from user
 			System.out.println("Enter the number");
 			num=sc.nextInt();
+			//checking if number is even or odd
 			if(num%2==0) {
+				//if even add the number in evensum integer
 				evensum+=num;
 			}
 			else {
+				//if odd add the number in oddsum integer
 				oddsum+=num;
 			}
+			//asking user if he/she wants to add another number
 			System.out.println("Do you want to add another number? y/n");
 			ans=sc.next().charAt(0);
 		}
 		while(ans=='Y'||ans=='y');
+		//if ans is no or n then loop ends and all the updated values are displayed
 		System.out.println("Addition of all Even Numbers is : "+evensum);
 		System.out.println("Addition of all Odd Numbers is : "+oddsum);
 	}
