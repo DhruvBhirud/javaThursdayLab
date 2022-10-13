1a457be598bc57758d6119b6d8571440d0164d99
DhruvBhirud <bhiruddhruv@gmail.com>
2022-10-13 19:27:13 +0530

    Updated Thursday lab 13.10.2022

---
+++ 03.LEC.13.10.2022/multiLevelInheritance/run.java
@@ -0,0 +1,12 @@
+package multiLevelInheritance;
+
+//WAP to create a class for Reverse No, second class Sum of Digit, Third class use both the class property and display the output
+public class run extends sumdig {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		rev();
+		sumdigit();
+	}
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 For Daily Assignments Visit -> [Daily Java Assignments](https://github.com/DhruvBhirud/dailyJavaAssignments)
 1. [LEC.22.9.2022](01.LEC.22.9.2022/)
 2. [LEC.29.9.2022](02.LEC.29.9.2022/)
+3. [LEC.13.10.2022](03.LEC.13.10.2022/)
