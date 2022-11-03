89ff9de74c602bee5abcdaa07c073bc48469a11d
DhruvBhirud <bhiruddhruv@gmail.com>
2022-11-03 22:35:13 +0530

    Updated Thursday Lab 03.11.2022

--- 06.LEC.03.11.2022/prodConsSync/Shop.java
+++ 06.LEC.03.11.2022/prodConsSync/Shop.java
@@ -1,10 +1,18 @@
 package prodConsSync;
+
+/*WAP using Synchronized Threads, which demonstrates Producer Consumer concept.
+Producer Consumer problem: The producer-consumer problem is the classical concurrency of a multi process synchronization problem.
+It is also known as bound-buffer problem. The problem describes two processes, the producer and the consumer, who share a common,
+fixed-size buffer used as a queue. The producer generates a piece of data, put it into the buffer and starts again.*/
+
 class Shop
 {
       private int materials;
       private boolean available = false;
+      //method to put the materials into the Shop
       public synchronized int get()
       {
+            //if no materials are available, wait
             while (available == false)
             {
                   try
@@ -15,12 +23,15 @@ class Shop
                   {
                   }
             }
+            //if materials are available, get them
             available = false;
+            //notify the producer that the materials have been taken
             notifyAll();
             return materials;
       }
       public synchronized void put(int value)
       {
+            //if materials are available, wait
             while (available == true)
             {
                   try
@@ -32,8 +43,11 @@ class Shop
                         ie.printStackTrace();
                   }
             }
+            //if no materials are available, put the materials
             materials = value;
+            //set the flag to true to indicate that materials are available
             available = true;
+            //notify the consumer that the materials are available
             notifyAll();
       }
 }
--- 06.LEC.03.11.2022/threadFibRev/Fib.java
+++ 06.LEC.03.11.2022/threadFibRev/Fib.java
@@ -1,12 +1,20 @@
 package threadFibRev;
+
+/*WAP which will generate the threads: 
+- To display 10 terms of Fibonacci series.
+- To display 1 to 10 in reverse order.*/
+
 class Fib extends Thread
 {
+     //method to display the fibonacci series
      public void run()
      {
           try
           {
+               //variable to store the first number
                int a=0, b=1, c=0, n=10;
                System.out.println("Fibonacci series first 10 terms are :");
+               //loop to display the fibonacci series
                while (n>0)
                {
                     System.out.print(c+" ");
@@ -16,6 +24,7 @@ class Fib extends Thread
                     n=n-1;
                }
           }
+          //catch exception
           catch (Exception ex)
           {
                ex.printStackTrace();
